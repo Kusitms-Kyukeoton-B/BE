f90b308de3d743182d8d5237451d073ad2890bc0
chahyunsoo <chahs1012@naver.com>
2024-04-03 17:41:59 +0900

    [feat] : CorsConfig 추가 및 안쓰는 코드 주석

--- src/main/java/com/kyukeoton_b/kyukeotonB_be/config/WebConfig.java
+++ src/main/java/com/kyukeoton_b/kyukeotonB_be/config/WebConfig.java
@@ -1,21 +1,21 @@
-package com.kyukeoton_b.kyukeotonB_be.config;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-@RequiredArgsConstructor
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .exposedHeaders("X-AUTH-TOKEN")
-                .allowCredentials(true)
-                .allowedOrigins("http://localhost:8080") //여기 수정
-                .allowedMethods("*");
-    }
-
-}
+//package com.kyukeoton_b.kyukeotonB_be.config;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//@RequiredArgsConstructor
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .exposedHeaders("X-AUTH-TOKEN")
+//                .allowCredentials(true)
+//                .allowedOrigins("http://localhost:8080") //여기 수정
+//                .allowedMethods("*");
+//    }
+//
+//}
--- src/main/java/com/kyukeoton_b/kyukeotonB_be/domain/user/UserService.java
+++ src/main/java/com/kyukeoton_b/kyukeotonB_be/domain/user/UserService.java
@@ -1,10 +1,10 @@
-package com.kyukeoton_b.kyukeotonB_be.domain.user;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-@Slf4j
-@Service
-@RequiredArgsConstructor
-public class UserService {
-}
+//package com.kyukeoton_b.kyukeotonB_be.domain.user;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Service;
+//@Slf4j
+//@Service
+//@RequiredArgsConstructor
+//public class UserService {
+//}
