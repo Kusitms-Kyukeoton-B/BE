40be726dd23a3c88c549c545e37994555cc69c3b
chahyunsoo <chahs1012@naver.com>
2024-04-03 18:19:15 +0900

    [fix] : 파일 위치 수정

--- src/main/java/com/kyukeoton_b/kyukeotonB_be/healthController.java
+++ src/main/java/com/kyukeoton_b/kyukeotonB_be/healthController.java
@@ -1,4 +1,4 @@
-package com.kyukeoton_b.kyukeotonB_be.domain;
+package com.kyukeoton_b.kyukeotonB_be;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
