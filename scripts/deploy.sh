90679882423bd2a02623e1b0546c52a0f56d7092
chahyunsoo <chahs1012@naver.com>
2024-04-03 18:35:42 +0900

    [fix] : 코드 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,47 +5,47 @@ DEFAULT_CONF=" /etc/nginx/nginx.conf"
 
 # blue가 실행 중이면 green을 up합니다.
 if [ -z $IS_GREEN_EXIST ];then
-  echo "### BLUE => GREEN ####"
-  echo ">>> green image를 pull합니다."
-  docker-compose pull green
-  echo ">>> green container를 up합니다."
-  docker-compose up -d green
+  sudo echo "### BLUE => GREEN ####"
+  sudo echo ">>> green image를 pull합니다."
+  sudo docker-compose pull green
+  sudo echo ">>> green container를 up합니다."
+  sudo docker-compose up -d green
   while [ 1 = 1 ]; do
-    echo ">>> green health check 중..."
+    sudo echo ">>> green health check 중..."
     sleep 3
     REQUEST=$(curl http://127.0.0.1:8082)
     if [ -n "$REQUEST" ]; then
-      echo ">>> 🍃 health check success !"
+      sudo echo ">>> 🍃 health check success !"
       break;
     fi
   done;
   sleep 3
-  echo ">>> nginx를 다시 실행 합니다."
+  sudo echo ">>> nginx를 다시 실행 합니다."
   sudo cp /etc/nginx/nginx.green.conf /etc/nginx/nginx.conf
   sudo nginx -s reload
-  echo ">>> blue container를 down합니다."
-  docker-compose stop blue
+  sudo echo ">>> blue container를 down합니다."
+  sudo docker-compose stop blue
 
 # green이 실행 중이면 blue를 up합니다.
 else
-  echo "### GREEN => BLUE ###"
-  echo ">>> blue image를 pull합니다."
-  docker-compose pull blue
-  echo ">>> blue container up합니다."
-  docker-compose up -d blue
+  sudo echo "### GREEN => BLUE ###"
+  sudo echo ">>> blue image를 pull합니다."
+  sudo docker-compose pull blue
+  sudo echo ">>> blue container up합니다."
+  sudo docker-compose up -d blue
   while [ 1 = 1 ]; do
-    echo ">>> blue health check 중..."
+    sudo echo ">>> blue health check 중..."
     sleep 3
     REQUEST=$(curl http://127.0.0.1:8081)
     if [ -n "$REQUEST" ]; then
-      echo ">>> 🍃 health check success !"
+      sudo echo ">>> 🍃 health check success !"
       break;
     fi
   done;
   sleep 3
-  echo ">>> nginx를 다시 실행 합니다."
+  sudo echo ">>> nginx를 다시 실행 합니다."
   sudo cp /etc/nginx/nginx.blue.conf /etc/nginx/nginx.conf
   sudo nginx -s reload
-  echo ">>> green container를 down합니다."
-  docker-compose stop green
+  sudo echo ">>> green container를 down합니다."
+  sudo docker-compose stop green
 fi
