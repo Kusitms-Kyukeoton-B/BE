233c60d67728c0ba5ab8a8f8a7aa4a01a46706a1
chahyunsoo <chahs1012@naver.com>
2024-04-05 12:48:38 +0900

    [chore] : 배포 확인

--- src/main/java/com/kyukeoton_b/kyukeotonB_be/healthController.java
+++ src/main/java/com/kyukeoton_b/kyukeotonB_be/healthController.java
@@ -2,11 +2,12 @@ package com.kyukeoton_b.kyukeotonB_be;
 
 import com.kyukeoton_b.kyukeotonB_be.global.ApiResponse;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class healthController {
-    @GetMapping("/")
+    @GetMapping("/user")
     public ApiResponse<String> hello() {
         return ApiResponse.onSuccess("Hello, Kyukeoton and Retest");
     }
