d89c237f96d8f08f3d0a5f1e3347ad4449301289
chahyunsoo <chahs1012@naver.com>
2024-04-06 18:55:03 +0900

    [fix] : 테스트

--- src/main/java/com/kyukeoton_b/kyukeotonB_be/KyukeotonBBeApplication.java
+++ src/main/java/com/kyukeoton_b/kyukeotonB_be/KyukeotonBBeApplication.java
@@ -4,13 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
 @EnableJpaAuditing
-@SpringBootApplication
-//@SpringBootApplication( exclude = {
-//		org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
-//		org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
-//		org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class }
-//)
+//@SpringBootApplication
+@SpringBootApplication( exclude = {
+		org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
+		org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
+		org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class }
+)
 public class KyukeotonBBeApplication {
 
 	public static void main(String[] args) {
