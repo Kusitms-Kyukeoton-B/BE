3b4064d3030cb0e17b719d5c932401646193b7fa
chahyunsoo <chahs1012@naver.com>
2024-04-07 04:23:16 +0900

    [feat] : 1차 배포완료

--- src/main/java/com/kyukeoton_b/kyukeotonB_be/global/config/CorsConfig.java
+++ src/main/java/com/kyukeoton_b/kyukeotonB_be/global/config/CorsConfig.java
@@ -1,25 +1,27 @@
-//package com.kyukeoton_b.kyukeotonB_be.global.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-//import org.springframework.web.filter.CorsFilter;
-//
-//import java.util.Arrays;
-//
-//@Configuration
-//public class CorsConfig {
-//    @Bean
-//    public CorsFilter corsFilter() {
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        CorsConfiguration config = new CorsConfiguration();
-//        config.setAllowCredentials(true);
+package com.kyukeoton_b.kyukeotonB_be.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
 //        config.addAllowedOrigin("http://localhost:5173");
-//        config.addAllowedOrigin("https://kusitms-kyukeoton-b.github.io/FE");
-//        config.addAllowedHeader("*");
-//        config.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-//        source.registerCorsConfiguration("/**",config);
-//        return new CorsFilter(source);
-//    }
-//}
+//        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        source.registerCorsConfiguration("/**",config);
+        return new CorsFilter(source);
+    }
+}
