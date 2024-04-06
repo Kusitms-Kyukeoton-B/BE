6498a6ed7af7843526cafdee3d39fda82d93a59e
chahyunsoo <chahs1012@naver.com>
2024-04-07 04:49:53 +0900

    [feat] : 2차 배포완료

--- src/main/java/com/kyukeoton_b/kyukeotonB_be/global/config/CorsConfig.java
+++ src/main/java/com/kyukeoton_b/kyukeotonB_be/global/config/CorsConfig.java
@@ -2,26 +2,44 @@ package com.kyukeoton_b.kyukeotonB_be.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 
-import java.util.Arrays;
+import java.util.Collections;
+
 
 @Configuration
+@EnableWebSecurity
 public class CorsConfig {
+
+    // ⭐️ CORS 설정
+    CorsConfigurationSource corsConfigurationSource() {
+        return request -> {
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowedHeaders(Collections.singletonList("*"));
+            config.setAllowedMethods(Collections.singletonList("*"));
+            config.setAllowedOriginPatterns(Collections.singletonList("*")); // ⭐️ 허용할 origin
+            config.setAllowCredentials(true);
+            return config;
+        };
+    }
+
     @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-//        config.addAllowedOrigin("http://localhost:5173");
-//        config.addAllowedOrigin("http://localhost:5173");
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-        source.registerCorsConfiguration("/**",config);
-        return new CorsFilter(source);
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.
+                httpBasic(HttpBasicConfigurer::disable)
+                .cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource())) // ⭐️⭐️⭐️
+                .csrf(AbstractHttpConfigurer::disable);
+//                .authorizeHttpRequests(authorize ->
+//                        authorize
+//                                .requestMatchers("/**").permitAll()
+//                );
+
+        return httpSecurity.build();
     }
 }
